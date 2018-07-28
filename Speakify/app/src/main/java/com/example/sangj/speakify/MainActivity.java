3e345efed4df0be74773686fc75e888b1292295a
betakuwe <33549490+betakuwe@users.noreply.github.com>
2018-07-28 14:09:13 +0800

    added filler counters
    added filler counters and default keywords

--- Speakify/app/src/main/java/com/example/sangj/speakify/MainActivity.java
+++ Speakify/app/src/main/java/com/example/sangj/speakify/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.sangj.speakify;
 
 import android.content.ActivityNotFoundException;
@@ -8,15 +9,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView mVoiceInputTv;
+    private TextView mFillerCountResult;
     private ImageButton mSpeakBtn;
+    private HashMap<String, Integer> fillerWordCount;
+    private String[] fillerWords;
 
 
     @Override
@@ -24,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fillerWordCount = new HashMap<>();
+        fillerWords = getResources().getStringArray(R.array.default_filler_words);
         mVoiceInputTv = findViewById(R.id.voiceInput);
+        mFillerCountResult = findViewById(R.id.fillerCount);
         mSpeakBtn = findViewById(R.id.btnSpeak);
         mSpeakBtn.setOnClickListener(new View.OnClickListener() {
 
@@ -38,12 +50,13 @@ public class MainActivity extends AppCompatActivity {
     private void startVoiceInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        // intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
-
+            Toast.makeText(this, "Your device does not support speech input.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -53,13 +66,48 @@ public class MainActivity extends AppCompatActivity {
 
         switch (requestCode) {
             case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
+                if (resultCode == RESULT_OK && data != null) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    mVoiceInputTv.setText(result.get(0));
+                    String resultString = result.get(0);
+                    mVoiceInputTv.setText(resultString);
+
+                    countFillerWords(resultString, fillerWords);
+
+                    mFillerCountResult.setText(fillerCountMessage(fillerWords));
                 }
                 break;
             }
-
         }
     }
+
+    private void countFillerWords(String message, String[] fillerWordArray) {
+        LinkedList<String> messageLL = new LinkedList<>(Arrays.asList(message.split(" ")));
+        for (String filler : fillerWordArray) {
+            int count = 0;
+            ListIterator<String> iter = messageLL.listIterator();
+            while(iter.hasNext()) {
+                if(iter.next().equals(filler)) {
+                    count++;
+                    iter.remove();
+                }
+            }
+            fillerWordCount.put(filler, count);
+        }
+    }
+
+    private String fillerCountMessage(String[] fillerWordArray) {
+        StringBuilder message = new StringBuilder();
+        for (String filler:fillerWordArray) {
+            int count = fillerWordCount.get(filler);
+            if (count > 0) {
+                message.append("\'");
+                message.append(filler);
+                message.append("\' : ");
+                message.append(count);
+                message.append("\n");
+            }
+        }
+        return message.toString();
+    }
 }
+
